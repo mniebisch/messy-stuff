801910d4d3e1e32a2d50d04593c9095f5e8e2865
mniebisch <michael.niebisch89@gmail.com>
2023-07-23 16:16:26 +0200

    Add tqdm to track progress during an epoch.

--- train.py
+++ train.py
@@ -1,11 +1,14 @@
 import pathlib
 from collections import OrderedDict
 
+import pandas as pd
 import torch
 import torch.nn as nn
 import torch.optim as optim
+import pyarrow.parquet as pq
 from torchdata import dataloader2
-
+from tqdm import tqdm
+from multiprocessing import Pool, cpu_count
 
 from pipeline_frame import load_data_framewise
 
@@ -52,6 +55,59 @@ def filter_state_dict_by_prefix(state_dict: OrderedDict[str, torch.Tensor], pref
 
     return filtered_state_dict
 
+
+def count_frames_in_csv(csv_file_path: str) -> int:
+    """
+    Count the total number of frames in the Parquet files specified in the CSV file.
+
+    Args:
+        csv_file_path (str or pathlib.Path): The path to the CSV file containing 'path' column with Parquet file paths.
+
+    Returns:
+        int: The total number of frames calculated from the Parquet files.
+    """
+    data_path = pathlib.Path(csv_file_path).parent
+
+    # Read the CSV file
+    train_df = pd.read_csv(csv_file_path)
+
+    num_frames = 0
+    for _, row in tqdm(train_df.iterrows(), total=len(train_df)):
+        file_meta = pq.read_metadata(data_path / row['path'])
+        num_frames += file_meta.num_rows // 543
+
+    return num_frames
+
+def process_file(row_data):
+    file_path, num_frames_per_row = row_data
+    file_meta = pq.read_metadata(file_path)
+    return file_meta.num_rows // num_frames_per_row
+
+def count_frames_in_csv_parallel(csv_file_path: str) -> int:
+    """
+    Count the total number of frames in the Parquet files specified in the CSV file using parallel processing.
+
+    Args:
+        csv_file_path (str or pathlib.Path): The path to the CSV file containing 'path' column with Parquet file paths.
+
+    Returns:
+        int: The total number of frames calculated from the Parquet files.
+    """
+    data_path = pathlib.Path(csv_file_path).parent
+
+    # Read the CSV file
+    train_df = pd.read_csv(csv_file_path)
+
+    num_frames_per_row = 543
+
+    num_workers = min(cpu_count(), len(train_df))
+    with Pool(num_workers) as pool:
+        row_data = [(data_path / row['path'], num_frames_per_row) for _, row in train_df.iterrows()]
+        with tqdm(total=len(train_df), desc="Processing", unit="file") as pbar:
+            results = list(tqdm(pool.imap_unordered(process_file, row_data), total=len(train_df), position=1, leave=False))
+
+    return sum(results)
+
 # Set the number of features and encoding dimension
 input_size = 3 * 543
 encoding_dim = 128
@@ -75,27 +131,33 @@ batch_size = 1024
 train_pipe = load_data_framewise(csv_file=train_csv, data_path=data_path, batch_size=batch_size)
 train_loader = dataloader2.DataLoader2(train_pipe)
 
-# Train the autoencoder
-num_epochs = 0
-for epoch in range(num_epochs):
-    print(epoch)
-    for data in train_loader:
-        img = data
-        
-        # Move the input data to the GPU
-        img = img.to(device)
+# Number of frames in overall dataset (extracted from count_frames)
+num_frames = count_frames_in_csv(train_csv)
+num_frames = count_frames_in_csv_parallel(train_csv)
 
-        # Forward pass
-        output = model(img)
-        loss = criterion(output, img)
-        
-        # Backward pass and optimization
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-        
-    # Print the loss after each epoch
-    print(f"Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item()}")
+# Train the autoencoder
+num_epochs = 3
+for epoch in range(num_epochs):
+    with tqdm(desc="Processing", unit="iter", position=0, leave=True, total=num_frames // batch_size) as pbar:
+        for data in train_loader:
+            img = data
+            
+            # Move the input data to the GPU
+            img = img.to(device)
+
+            # Forward pass
+            output = model(img)
+            loss = criterion(output, img)
+            
+            # Backward pass and optimization
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            pbar.update(1)
+
+            
+        # Print the loss after each epoch
+        # print(f"Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item()}")
 
 # Save the encoder weights
 model_state_dict = model.state_dict()
