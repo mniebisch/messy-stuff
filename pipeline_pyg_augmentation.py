4371e22d3946cd82a9a8bf4d1d186c304871f1c3
Michael Niebisch <michael.niebisch89@gmail.com>
2023-09-03 22:36:12 +0200

    Add batched version of pyg transformations.

--- pipeline_pyg_augmentation.py
+++ pipeline_pyg_augmentation.py
@@ -1,25 +1,9 @@
-from typing import Optional, Union
+from typing import List, Optional
 
 import torch
 import torch_geometric.data as pyg_data
 import torch_geometric.transforms as pyg_transforms
 import torchdata
-
-
-def geometric_augmentation(
-    datapipe: Union[
-        torchdata.datapipes.iter.IterDataPipe, torchdata.datapipes.map.MapDataPipe
-    ],
-    transforms: pyg_transforms.BaseTransform,
-) -> torchdata.datapipes.map.MapDataPipe:
-    # TODO at the moment function needs to be applied before batching
-    # make more generic
-    # TODO if unbatched version is applied maybe internally apply batching anyway
-    # to save computational cost? (be aware  of premature optimization!)
-    datapipe = datapipe.map(create_geom_datapoint)
-    datapipe = datapipe.map(transforms)
-    datapipe = datapipe.map(unwrap_pyg_datapoint)
-    return datapipe
 
 
 def create_geom_datapoint(inputs: torch.Tensor) -> pyg_data.Data:
@@ -34,7 +18,12 @@ def unwrap_pyg_datapoint(inputs: pyg_data.Data) -> torch.Tensor:
     return torch.reshape(inputs.pos, (-1,))
 
 
+def collate_pyg_datapoint(batch: List[pyg_data.Data]) -> torch.Tensor:
+    return torch.stack([unwrap_pyg_datapoint(sample) for sample in batch])
+
+
 if __name__ == "__main__":
+    # TODO how are pyg transforms applied? does the current code lead to a speed up?
 
     def create_prototyping_pipe(
         samples: torch.Tensor,
@@ -43,19 +32,31 @@ if __name__ == "__main__":
         transforms: Optional[pyg_transforms.BaseTransform] = None,
     ) -> torchdata.datapipes.iter.IterDataPipe:
         datapipe = torchdata.datapipes.iter.IterableWrapper(samples)
+
         if transforms is not None:
-            datapipe = geometric_augmentation(datapipe=datapipe, transforms=transforms)
+            datapipe = datapipe.map(create_geom_datapoint)
+
+        if transforms is not None:
+            datapipe = datapipe.map(transforms)
+
         datapipe = datapipe.batch(batch_size=batch_size, drop_last=drop_last_batch)
-        datapipe = datapipe.collate()
+
+        # if transforms is not None:
+        #     datapipe = datapipe.map(transforms)
+
+        if transforms is not None:
+            datapipe = datapipe.collate(collate_fn=collate_pyg_datapoint)
+        else:
+            datapipe = datapipe.collate()
         return datapipe
 
     sample_data = torch.rand(20, 21 * 3, dtype=torch.float32)
-    sample_data = torch.zeros((20, 21 * 3), dtype=torch.float32) + 1
+    sample_data = torch.zeros((10000, 21 * 3), dtype=torch.float32) + 1
     transforms = pyg_transforms.Compose(
         [
-            # pyg_transforms.NormalizeScale(),
+            pyg_transforms.NormalizeScale(),
             pyg_transforms.RandomFlip(axis=0, p=0.5),
-            # pyg_transforms.RandomJitter(0.01),
+            pyg_transforms.RandomJitter(0.01),
         ]
     )
     # transforms = pyg_transforms.Compose(
@@ -65,9 +66,8 @@ if __name__ == "__main__":
     #         pyg_transforms.RandomRotate(degrees=25, axis=2),
     #     ]
     # )
-    # pipe = geometric_augmentation(samples=sample_data, transforms=transforms)
     pipe = create_prototyping_pipe(
-        samples=sample_data, batch_size=5, drop_last_batch=False, transforms=transforms
+        samples=sample_data, batch_size=64, drop_last_batch=False, transforms=transforms
     )
     for sample in pipe:
         print("blub")
