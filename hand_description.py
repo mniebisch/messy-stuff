e64c58ce5ad0db7a0b907275a8737f6cc5ac5354
mniebisch <michael.niebisch89@gmail.com>
2023-10-08 11:47:56 +0200

    Add function from stackoverflow to compute angle between vectors.

--- hand_description.py
+++ hand_description.py
@@ -28,3 +28,25 @@ def compute_palm_direction(hand: npt.NDArray) -> tuple[float, float, float]:
 
     palm_direction = np.cross(wrist_index_direction, wrist_pinky_direction)
     return tuple(palm_direction)
+
+
+def unit_vector(vector: npt.NDArray) -> npt.NDArray:
+    """Returns the unit vector of the vector."""
+    return vector / np.linalg.norm(vector)
+
+
+def angle_between(v1: npt.NDArray, v2: npt.NDArray) -> float:
+    """Returns the angle in radians between vectors 'v1' and 'v2'::
+
+    Source: https://stackoverflow.com/a/13849249
+
+    >>> angle_between((1, 0, 0), (0, 1, 0))
+    1.5707963267948966
+    >>> angle_between((1, 0, 0), (1, 0, 0))
+    0.0
+    >>> angle_between((1, 0, 0), (-1, 0, 0))
+    3.141592653589793
+    """
+    v1_u = unit_vector(v1)
+    v2_u = unit_vector(v2)
+    return np.arccos(np.clip(np.dot(v1_u, v2_u), -1.0, 1.0))
