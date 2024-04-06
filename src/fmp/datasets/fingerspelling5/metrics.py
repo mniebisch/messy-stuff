752e17e3a904be427bc8a48c25c31f71e6744eb0
mniebisch <michael.niebisch89@gmail.com>
2024-04-06 12:44:01 +0200

    Add implementation to compute distances between landmarks.

--- src/fmp/datasets/fingerspelling5/metrics.py
+++ src/fmp/datasets/fingerspelling5/metrics.py
@@ -10,10 +10,13 @@ from . import utils
 
 __all__ = [
     "compute_hand_extend",
+    "compute_distances_mean",
+    "compute_distances_std",
     "compute_hand_mean",
     "compute_hand_std",
     "compute_hand_plane_area",
     "compute_hand_plane_perimeter",
+    "distance_wrapper",
     "location_wrapper",
     "space_wrapper",
 ]
@@ -73,6 +76,23 @@ def space_wrapper(func):
     return wrap_values
 
 
+def distance_wrapper(func):
+    # TODO register allowed functions and verify on use?
+    def wrap_values(*args, **kwargs):
+        signature = inspect.signature(func)
+        bound = signature.bind(*args, **kwargs)
+        bound.apply_defaults()
+
+        part = bound.arguments["part"]
+        metric_type = func.__name__.split("_")[-1]
+        axis = bound.arguments["axis"]
+
+        value = func(*args, **kwargs)
+        return {f"{part}_{axis}_{metric_type}": value}
+
+    return wrap_values
+
+
 @check_hand_landmark_shape
 def compute_hand_extend(hand: npt.NDArray, part: str = "all") -> npt.NDArray:
     part_indices = getattr(utils.mediapipe_hand_landmarks.parts, part)
@@ -96,17 +116,23 @@ def compute_hand_std(hand: npt.NDArray, part: str = "all") -> npt.NDArray:
     return np.std(hand_part, axis=0)
 
 
-@check_hand_landmark_shape
-def compute_hand_plane_perimeter(
-    hand: npt.NDArray, plane: tuple[str, str], part: str = "all"
-) -> float:
+def _compute_hand_convex_hull(
+    hand: npt.NDArray, plane: tuple[str, str], part: str
+) -> spatial.ConvexHull:
     part_indices = getattr(utils.mediapipe_hand_landmarks.parts, part)
     plane_ind = (
         utils.mediapipe_hand_landmarks.spatial_coords.index(plane[0]),
         utils.mediapipe_hand_landmarks.spatial_coords.index(plane[1]),
     )
     hand_part = hand[np.ix_(part_indices, plane_ind)]
-    convex_hull = spatial.ConvexHull(hand_part)
+    return spatial.ConvexHull(hand_part)
+
+
+@check_hand_landmark_shape
+def compute_hand_plane_perimeter(
+    hand: npt.NDArray, plane: tuple[str, str], part: str = "all"
+) -> float:
+    convex_hull = _compute_hand_convex_hull(hand, plane, part)
     return convex_hull.area
 
 
@@ -114,13 +140,7 @@ def compute_hand_plane_perimeter(
 def compute_hand_plane_area(
     hand: npt.NDArray, plane: tuple[str, str], part: str = "all"
 ) -> float:
-    part_indices = getattr(utils.mediapipe_hand_landmarks.parts, part)
-    plane_ind = (
-        utils.mediapipe_hand_landmarks.spatial_coords.index(plane[0]),
-        utils.mediapipe_hand_landmarks.spatial_coords.index(plane[1]),
-    )
-    hand_part = hand[np.ix_(part_indices, plane_ind)]
-    convex_hull = spatial.ConvexHull(hand_part)
+    convex_hull = _compute_hand_convex_hull(hand, plane, part)
     vertices = convex_hull.points[convex_hull.vertices]
     return shoelace_formula(vertices)
 
@@ -167,3 +187,27 @@ def shoelace_formula(vertices: npt.NDArray) -> float:
     x = vertices[:, 0]
     y = vertices[:, 1]
     return 0.5 * np.abs(np.dot(x, np.roll(y, 1)) - np.dot(y, np.roll(x, 1)))
+
+
+def _compute_distances(hand: npt.NDArray, axis: str, part: str) -> npt.NDArray:
+    plane_ind = [utils.mediapipe_hand_landmarks.spatial_coords.index(ax) for ax in axis]
+    part_indices = getattr(utils.mediapipe_hand_landmarks.parts, part)
+    hand_part = hand[np.ix_(part_indices, plane_ind)]
+    dist = distance.cdist(hand_part, hand_part, metric="euclidean")
+    assert dist.shape[0] == dist.shape[1], "dist has to be a square array!"
+    rows, cols = np.triu_indices_from(dist, k=1)
+    return dist[rows, cols]
+
+
+@check_hand_landmark_shape
+def compute_distances_mean(hand: npt.NDArray, axis: str, part: str = "all") -> float:
+    # valid axis ("xyz", "xy", "yz", "xz", "x", "y", "z")
+    dist_flat = _compute_distances(hand, axis, part)
+    return np.mean(dist_flat)
+
+
+@check_hand_landmark_shape
+def compute_distances_std(hand: npt.NDArray, axis: str, part: str = "all") -> float:
+    # valid axis ("xyz", "xy", "yz", "xz", "x", "y", "z")
+    dist_flat = _compute_distances(hand, axis, part)
+    return np.std(dist_flat)
--- tests/fmp/datasets/fingerspelling5/test_metrics.py
+++ tests/fmp/datasets/fingerspelling5/test_metrics.py
@@ -98,3 +98,43 @@ def test_plane_wrap():
     expected = {"pinky_xy_area": 1.0}
     output = wrapped_func(hand, plane, part)
     assert output == pytest.approx(expected)
+
+
+def test_compute_distances_mean_1d():
+    hand = np.zeros((21, 3))
+    part = "thumb"
+    axis = "x"
+    hand[1, 0] = 1
+    expected = 3 / 6
+    output = metrics.compute_distances_mean(hand, axis, part)
+    assert output == pytest.approx(expected)
+
+
+def test_compute_distances_mean_2d():
+    hand = np.zeros((21, 3))
+    part = "index_finger"
+    axis = "yz"
+    hand[5, 1] = 1
+    expected = 3 / 6
+    output = metrics.compute_distances_mean(hand, axis, part)
+    assert output == pytest.approx(expected)
+
+
+def test_compute_distances_mean_3d():
+    hand = np.zeros((21, 3))
+    axis = "xyz"
+    hand[0, 0] = 1
+    expected = 20 / 210
+    output = metrics.compute_distances_mean(hand, axis)
+    assert output == pytest.approx(expected)
+
+
+def test_distance_wrap():
+    func = metrics.distance_wrapper(metrics.compute_distances_mean)
+    hand = np.zeros((21, 3))
+    part = "index_finger"
+    axis = "yz"
+    hand[5, 1] = 1
+    expected = {"index_finger_yz_mean": 3 / 6}
+    output = func(hand, axis, part)
+    assert output == pytest.approx(expected)
