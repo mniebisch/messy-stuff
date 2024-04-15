7b4aa704855d2815fc4b677e61d654f4d472e1b3
mniebisch <michael.niebisch89@gmail.com>
2024-04-15 16:54:25 +0200

    Add loading of predictions.

--- apps/visualize_fingerspelling5.py
+++ apps/visualize_fingerspelling5.py
@@ -12,7 +12,8 @@ predictions_path = root_path / "predictions"
 metrics_scaled_file = metrics_path / "fingerspelling5_singlehands_scaled.csv"
 metrics_unscaled_file = metrics_path / "fingerspelling5_singlehands.csv"
 fingerspelling_file = data_path / "fingerspelling5_singlehands.csv"
-predictions_file = None  # TODO
+# is going to change the most?
+predictions_file = predictions_path / "prediction__version_22__epoch=17-step=36.csv"
 
 
 metrics_scaled = pd.read_csv(metrics_scaled_file)
@@ -22,11 +23,13 @@ scale_col = "scaled"
 metrics_scaled[scale_col] = True
 metrics_unscaled[scale_col] = False
 
-metrics = pd.concat([metrics_scaled, metrics_unscaled], axis=0).reset_index()
+metrics = pd.concat([metrics_scaled, metrics_unscaled], axis=0).reset_index(drop=True)
 
 fingerspelling_data = pd.read_csv(fingerspelling_file)
 fingerspelling_data = fingerspelling_data.loc[~fingerspelling_data.isnull().any(axis=1)]
-fingerspelling_data = fingerspelling_data.reset_index()
+fingerspelling_data = fingerspelling_data.reset_index(drop=True)
 
+
+predictions = pd.read_csv(predictions_file)
 
 print("Done")
