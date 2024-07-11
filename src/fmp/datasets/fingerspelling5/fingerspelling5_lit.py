ab9c2ab131023c8666db6e36d83172a3b2487e6f
mniebisch <michael.niebisch89@gmail.com>
2024-07-11 19:22:26 +0200

    Clean up class attribute typing. Use class attributes instead of lightnings 'hparams'.

--- src/fmp/datasets/fingerspelling5/fingerspelling5_lit.py
+++ src/fmp/datasets/fingerspelling5/fingerspelling5_lit.py
@@ -28,17 +28,24 @@ class Fingerspelling5LandmarkDataModule(L.LightningDataModule):
         super().__init__()
         self.save_hyperparameters()
 
-        self.fingerspelling5_csv = self.get_data_filename()
+        self.dataset_dir = dataset_dir
+        self.batch_size = batch_size
+        self.num_dataloader_workers = num_dataloader_workers
+        self.train_transforms = train_transforms
+        self.valid_transforms = valid_transforms
+        self.predict_transforms = predict_transforms
+        self.datasplit_file = datasplit_file
 
+        self.fingerspelling5_csv = self.get_data_filename()
         self.validate_dataset_dir()
 
-        if self.hparams.datasplit_file is not None:
-            self.validate_datasplit_file()
+        if self.datasplit_file is not None:
+            self.validate_datasplit_file(self.datasplit_file)
 
     def setup(self, stage: str):
         filter_nans = True
         if stage == "fit":
-            if self.hparams.datasplit_file is None:
+            if self.datasplit_file is None:
                 raise ValueError(
                     "Fit without 'datasplit_file' not possible. "
                     "Please provide 'datasplit_file'."
@@ -56,16 +63,16 @@ class Fingerspelling5LandmarkDataModule(L.LightningDataModule):
             valid_data = landmark_data.loc[val_index]
 
             self.train_data = fingerspelling5.Fingerspelling5Landmark(
-                train_data, transforms=self.hparams.train_transforms
+                train_data, transforms=self.train_transforms
             )
             self.valid_train_split = fingerspelling5.Fingerspelling5Landmark(
                 train_data,
-                transforms=self.hparams.valid_transforms,
+                transforms=self.valid_transforms,
                 split="train",
             )
             self.valid_valid_split = fingerspelling5.Fingerspelling5Landmark(
                 valid_data,
-                transforms=self.hparams.valid_transforms,
+                transforms=self.valid_transforms,
                 split="valid",
             )
 
@@ -77,7 +84,7 @@ class Fingerspelling5LandmarkDataModule(L.LightningDataModule):
             )
             self.predict_data = fingerspelling5.Fingerspelling5Landmark(
                 landmark_data,
-                transforms=self.hparams.predict_transforms,
+                transforms=self.predict_transforms,
             )
         else:
             pass
@@ -88,26 +95,26 @@ class Fingerspelling5LandmarkDataModule(L.LightningDataModule):
     def train_dataloader(self) -> torch_data.DataLoader:
         return torch_data.DataLoader(
             self.train_data,
-            batch_size=self.hparams.batch_size,
+            batch_size=self.batch_size,
             shuffle=True,
             drop_last=True,
-            num_workers=self.hparams.num_dataloader_workers,
+            num_workers=self.num_dataloader_workers,
         )
 
     def val_dataloader(self) -> List[torch_data.DataLoader]:
         train_loader = torch_data.DataLoader(
             self.valid_train_split,
-            batch_size=self.hparams.batch_size,
+            batch_size=self.batch_size,
             shuffle=False,
             drop_last=False,
-            num_workers=self.hparams.num_dataloader_workers,
+            num_workers=self.num_dataloader_workers,
         )
         valid_loader = torch_data.DataLoader(
             self.valid_valid_split,
-            batch_size=self.hparams.batch_size,
+            batch_size=self.batch_size,
             shuffle=False,
             drop_last=False,
-            num_workers=self.hparams.num_dataloader_workers,
+            num_workers=self.num_dataloader_workers,
         )
         return [train_loader, valid_loader]
 
@@ -123,7 +130,9 @@ class Fingerspelling5LandmarkDataModule(L.LightningDataModule):
             num_workers=self.hparams.num_dataloader_workers,
         )
 
-    def validate_dataset_dir(self) -> None:
+    def validate_dataset_dir(
+        self,
+    ) -> None:
         if not self.fingerspelling5_csv.exists():
             raise ValueError(
                 f"Expected dataset CSV '{str(self.fingerspelling5_csv)}' not found."
@@ -134,24 +143,23 @@ class Fingerspelling5LandmarkDataModule(L.LightningDataModule):
                 f"Dataset CSV '{str(self.fingerspelling5_csv)}' is not a file."
             )
 
-        dataset_path = pathlib.Path(self.hparams.dataset_dir)
+        dataset_path = pathlib.Path(self.dataset_dir)
         csv_data_files = _extract_data_csv_files(dataset_path)
 
         if len(csv_data_files) > 1:
             warnings.warn(
-                f"Multiple data CSVs found in {self.hparams.dataset_dir}. "
+                f"Multiple data CSVs found in {self.dataset_dir}. "
                 "The dataset directory is expected to have only one data CSV. "
                 "Please check directory to ensure correct data is used."
             )
 
-    def validate_datasplit_file(self) -> None:
-        datasplit_file = pathlib.Path(self.hparams.datasplit_file)
-
-        if not datasplit_file.exists():
-            raise ValueError(f"Invalid datasplit file '{str(datasplit_file)}'.")
+    @staticmethod
+    def validate_datasplit_file(datasplit_file: str) -> None:
+        if not pathlib.Path(datasplit_file).exists():
+            raise ValueError(f"Invalid datasplit file '{datasplit_file}'.")
 
     def validate_datasplit_data(self, landmark_data: pd.DataFrame) -> None:
-        split_data = pd.read_csv(self.hparams.datasplit_file)
+        split_data = pd.read_csv(self.datasplit_file)
 
         if len(split_data) != len(landmark_data):
             raise ValueError("Landmark data and split data are not of same length.")
@@ -165,8 +173,8 @@ class Fingerspelling5LandmarkDataModule(L.LightningDataModule):
             )
 
         if (
-            not "train" in split_data["split"].values
-            or not "valid" in split_data["split"].values
+            "train" not in split_data["split"].values
+            or "valid" not in split_data["split"].values
         ):
             raise ValueError(
                 "Expect the split identifiers 'train' and 'split' but didn't found them."
@@ -174,7 +182,7 @@ class Fingerspelling5LandmarkDataModule(L.LightningDataModule):
         # check if other elements than 'train' or 'valid' are in split col?
 
     def load_datasplit_indices(self) -> Tuple[npt.NDArray, npt.NDArray]:
-        split_data = pd.read_csv(self.hparams.datasplit_file)
+        split_data = pd.read_csv(self.datasplit_file)
 
         train_indices = split_data["split"] == "train"
         valid_indices = split_data["split"] == "valid"
@@ -182,11 +190,11 @@ class Fingerspelling5LandmarkDataModule(L.LightningDataModule):
         return train_indices.values, valid_indices.values
 
     def extract_dataset_name(self) -> str:
-        dataset_path = pathlib.Path(self.hparams.dataset_dir)
+        dataset_path = pathlib.Path(self.dataset_dir)
         return dataset_path.name
 
     def get_data_filename(self) -> pathlib.Path:
-        dataset_path = pathlib.Path(self.hparams.dataset_dir)
+        dataset_path = pathlib.Path(self.dataset_dir)
         dataset_name = self.extract_dataset_name()
         file_name = f"{dataset_name}.csv"
         return dataset_path / file_name
