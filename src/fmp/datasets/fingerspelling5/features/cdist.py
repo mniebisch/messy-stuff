b786fbde9f1c6ef23d37f6f2d33bec6e6d125335
mniebisch <michael.niebisch89@gmail.com>
2024-07-14 16:03:35 +0200

    Modify such that batched inputs can be processed, too.

--- src/fmp/datasets/fingerspelling5/features/cdist.py
+++ src/fmp/datasets/fingerspelling5/features/cdist.py
@@ -3,9 +3,27 @@ import torch
 __all__ = ["cdist_combination"]
 
 
+# TODO rename as cdist is not completely correct? rename to cdiff?
 def cdist_combination(spatial_coords: torch.Tensor) -> torch.Tensor:
-    num_values = len(spatial_coords)
+    if spatial_coords.dim() == 2:
+        num_values = len(spatial_coords)
+    elif spatial_coords.dim() == 3:
+        num_values = spatial_coords.shape[1]
+    else:
+        raise ValueError(
+            "Input has invalid number of dimensions. "
+            "Either inputs with shape [P x M] or shape[B x P x M] are allowed."
+        )
+
     combination_indices = torch.combinations(torch.arange(num_values), r=2)
-    left_values = spatial_coords[combination_indices[:, 0]]
-    right_values = spatial_coords[combination_indices[:, 1]]
+    left_indices = combination_indices[:, 0]
+    right_indices = combination_indices[:, 1]
+
+    if spatial_coords.dim() == 2:
+        left_values = spatial_coords[left_indices]
+        right_values = spatial_coords[right_indices]
+    else:
+        left_values = spatial_coords[:, left_indices]
+        right_values = spatial_coords[:, right_indices]
+
     return left_values - right_values
--- tests/fmp/datasets/fingerspelling5/features/test_cdist.py
+++ tests/fmp/datasets/fingerspelling5/features/test_cdist.py
@@ -1,3 +1,4 @@
+import pytest
 import torch
 
 from fmp.datasets.fingerspelling5.features import cdist
@@ -15,3 +16,26 @@ def test_three_points() -> None:
     expected = torch.Tensor([[-1, -4, 11], [0, 0, 4], [1, 4, -7]])
     output = cdist.cdist_combination(input_coords)
     torch.testing.assert_close(output, expected)
+
+
+def test_batched_input() -> None:
+    input_coords = torch.Tensor(
+        [
+            [[1, 0, 3], [2, 4, -8], [1, 0, -1]],
+            [[1, 2, 3], [-1, -2, -3], [1, 0, -1]],
+        ]
+    )
+    expected = torch.Tensor(
+        [
+            [[-1, -4, 11], [0, 0, 4], [1, 4, -7]],
+            [[2, 4, 6], [0, 2, 4], [-2, -2, -2]],
+        ]
+    )
+    output = cdist.cdist_combination(input_coords)
+    torch.testing.assert_close(output, expected)
+
+
+@pytest.mark.parametrize("input_coords", [torch.zeros(3), torch.zeros(1, 2, 3, 4)])
+def test_check_input_dimensions(input_coords) -> None:
+    with pytest.raises(ValueError, match="Input has invalid number of dimensions"):
+        cdist.cdist_combination(input_coords)
