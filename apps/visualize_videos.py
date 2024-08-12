3b1834c545bc0e874cac0025bf5346750de6f06f
mniebisch <michael.niebisch89@gmail.com>
2024-08-12 22:39:49 +0200

    Add command line argument parsing.

--- apps/visualize_videos.py
+++ apps/visualize_videos.py
@@ -1,5 +1,6 @@
 import pathlib
 
+import click
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
@@ -104,80 +105,109 @@ def draw_hand(canvas, landmarks):
     return canvas
 
 
-img_data_dir = pathlib.Path(__file__).parent.parent.parent.parent / "data"
-data_dir = pathlib.Path(__file__).parent.parent / "data" / "fingerspelling5"
-
-dataset_name = "fingerspelling5_singlehands_sorted"
-
-# data interface
-example_person = "D"
-example_letter = "r"
-
-other_data_file = data_dir / dataset_name / f"{dataset_name}.csv"
-other_data = fingerspelling5.utils.read_csv(other_data_file, filter_nans=True)
-other_selection = other_data.loc[
-    (other_data["person"] == example_person) & (other_data["letter"] == example_letter)
-]
-data_columns = fingerspelling5.utils.generate_hand_landmark_columns()
-raw_data = other_selection.loc[:, data_columns].values
-# TODO dangerous path!!! stacked variant vs single row variant are kinda duplicats
-raw_values_stacked = raw_data.reshape(
-    -1,
-    fingerspelling5.utils.mediapipe_hand_landmarks.num_nodes,
-    len(fingerspelling5.utils.mediapipe_hand_landmarks.spatial_coords),
+@click.command()
+@click.option(
+    "--img-data-dir",
+    required=True,
+    type=click.Path(
+        path_type=pathlib.Path, resolve_path=True, dir_okay=True, file_okay=False
+    ),
 )
-example_files = other_selection["img_file"]
+@click.option(
+    "--dataset-dir",
+    required=True,
+    type=click.Path(
+        path_type=pathlib.Path, resolve_path=True, dir_okay=True, file_okay=False
+    ),
+)
+@click.option("--dataset-name", required=True, type=str)
+@click.option("--person", required=True, type=str)
+@click.option("--letter", required=True, type=str)
+def main(
+    img_data_dir: pathlib.Path,
+    dataset_dir: pathlib.Path,
+    dataset_name: str,
+    person: str,
+    letter: str,
+):
+    """
+    ```
+    python apps/visualize_videos.py \
+        --img-data-dir ../../data/ \ 
+        --dataset-dir data/fingerspelling5 \
+        --dataset-name fingerspelling5_singlehands_sorted \
+        --person D \
+        --letter r
+    ```
+    """
+    data_dir = dataset_dir
+    example_person = person
+    example_letter = letter
 
-frames = []
-for example_file, example_values in zip(example_files, raw_values_stacked):
-    image_path = img_data_dir.joinpath(pathlib.Path(example_file))
-    img = io.imread(image_path)
-    frames.append((img, example_values))
+    other_data_file = data_dir / dataset_name / f"{dataset_name}.csv"
+    other_data = fingerspelling5.utils.read_csv(other_data_file, filter_nans=True)
+    other_selection = other_data.loc[
+        (other_data["person"] == example_person)
+        & (other_data["letter"] == example_letter)
+    ]
+    data_columns = fingerspelling5.utils.generate_hand_landmark_columns()
+    raw_data = other_selection.loc[:, data_columns].values
+    # TODO dangerous path!!! stacked variant vs single row variant are kinda duplicats
+    raw_values_stacked = raw_data.reshape(
+        -1,
+        fingerspelling5.utils.mediapipe_hand_landmarks.num_nodes,
+        len(fingerspelling5.utils.mediapipe_hand_landmarks.spatial_coords),
+    )
+    example_files = other_selection["img_file"]
+
+    frames = []
+    for example_file, example_values in zip(example_files, raw_values_stacked):
+        image_path = img_data_dir.joinpath(pathlib.Path(example_file))
+        img = io.imread(image_path)
+        frames.append((img, example_values))
+
+    # create cv2 window
+    cv2.namedWindow("muh")
+
+    def on_trackbar(val):
+        global current_frame
+        current_frame = val
+        img, values = frames[val]
+        img = draw_hand(img, values)
+
+        cv2.imshow("blub", img)
+
+    cv2.createTrackbar("oi", "muh", 0, len(frames) - 1, on_trackbar)
+
+    playing = False
+    current_frame = 0
+    while True:
+        if playing:
+
+            cv2.setTrackbarPos("oi", "muh", current_frame)
+            current_frame += 1
+
+            if current_frame > len(frames):
+                current_frame = 0
+
+        key = cv2.waitKey(150)
+
+        if key == ord("s"):
+            playing = False
+        elif key == ord("d"):
+            playing = True
+        elif key == ord("q"):
+            break
+
+    print("Done")
+
+    # TODO add hotkeys for image is "bad" -> should be written to csv or similar
+    # TODO if "bad" labeling available show as text somewhere?
+
+    # TODO add creation of csv file to log 'quality' if not existent
+    # TODO add 'quality' and 'view' information to csv, 'quality' via keystroke, and 'view' automatically
+    # TODO add vis of 'quality' in vid
 
 
-# create cv2 window
-cv2.namedWindow("muh")
-
-
-def on_trackbar(val):
-    global current_frame
-    current_frame = val
-    img, values = frames[val]
-    img = draw_hand(img, values)
-
-    cv2.imshow("blub", img)
-
-
-cv2.createTrackbar("oi", "muh", 0, len(frames) - 1, on_trackbar)
-
-playing = False
-current_frame = 0
-while True:
-    if playing:
-
-        cv2.setTrackbarPos("oi", "muh", current_frame)
-        current_frame += 1
-
-        if current_frame > len(frames):
-            current_frame = 0
-
-    key = cv2.waitKey(150)
-
-    if key == ord("s"):
-        playing = False
-    elif key == ord("d"):
-        playing = True
-    elif key == ord("q"):
-        break
-
-
-print("Done")
-
-
-# TODO add hotkeys for image is "bad" -> should be written to csv or similar
-# TODO if "bad" labeling available show as text somewhere?
-
-# TODO argparse/click/jsonargparse for command line handling
-# TODO add creation of csv file to log 'quality' if not existent
-# TODO add 'quality' and 'view' information to csv, 'quality' via keystroke, and 'view' automatically
-# TODO add vis of 'quality' in vid
+if __name__ == "__main__":
+    main()
