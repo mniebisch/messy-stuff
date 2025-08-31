9d986107226b131b5950e865669d6b4b5b1d87dc
mniebisch <michael.niebisch89@gmail.com>
2025-08-31 12:47:28 +0200

    Add provided modules and classes.

--- src/fmp/lit_tools/callbacks/__init__.py
+++ src/fmp/lit_tools/callbacks/__init__.py
@@ -1,4 +1,11 @@
+from . import aim
 from .fingerspelling5_metric_writer import Fingerseplling5MetricWriter
 from .fingerspelling5_prediction_writer import Fingerspelling5PredictionWriter
+from .image_stat_writer import ImageStatWriter
 
-__all__ = ["Fingerseplling5MetricWriter", "Fingerspelling5PredictionWriter"]
+__all__ = [
+    "aim",
+    "ImageStatWriter",
+    "Fingerseplling5MetricWriter",
+    "Fingerspelling5PredictionWriter",
+]
