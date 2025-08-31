45c3273be2a51593eae7b326a764b26fcf0f2f3b
mniebisch <michael.niebisch89@gmail.com>
2025-08-31 13:00:53 +0200

    Adapt lightning cli to aim config writer.

--- scripts/train_fingerspelling5_litcli.py
+++ scripts/train_fingerspelling5_litcli.py
@@ -1,10 +1,16 @@
+import torch
 from lightning.pytorch.cli import LightningCLI
 
-from fmp import datasets
+from fmp.lit_tools.callbacks.aim import AimConfigWriter
+
+torch.set_float32_matmul_precision("high")
 
 
 def cli_main():
-    cli = LightningCLI()
+    cli = LightningCLI(
+        save_config_callback=AimConfigWriter,
+        save_config_kwargs={"logdir": "./config_logs"},
+    )
 
 
 if __name__ == "__main__":
